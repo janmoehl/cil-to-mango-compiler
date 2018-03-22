34cf92d9d19dc262dbb434b0c89d145d95a2b61b
Jan Möhlmann <janmoehl@uni-bremen.de>
2018-03-22 13:27:04 +0100

    Initial Commit - Prints already the module names of the example program

---
+++ SimpleExample/SimpleExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SimpleExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ cilToMangoCompiler/cilToMangoCompiler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+
+namespace cilToMango
+{
+    public class MainClass
+    {
+
+        //private readonly string _targetFileName;
+        //private readonly ModuleDefinition _module;
+
+        public static void Main(string[] args)
+        {
+            string fileName = "../../../../SimpleExample/SimpleExample/bin/Debug/SimpleExample.exe";
+            ModuleDefinition module = ModuleDefinition.ReadModule(fileName);
+            Console.WriteLine("Opened " + fileName + " for reading");
+            Console.WriteLine("File has " + module.Types.Count + "Types:");
+            foreach (TypeDefinition type in module.Types)
+            {
+                Console.WriteLine(type.Name);
+            }
+        }
+    }
+}
