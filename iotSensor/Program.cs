3d4d201730b1b06eaf4f698cca709b854a69f395
Jan Möhlmann <janmoehl@uni-bremen.de>
2018-04-19 14:40:59 +0200

    added example for riot with syscalls

---
+++ iotSensor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace iotSensor
+{
+    class MainClass
+    {
+        static public int SYSgetSensorValue()
+        {
+            return 0;
+        }
+
+        static public void SYSprint(int i)
+        {
+            
+        }
+
+        static public void Main()
+        {
+				    String hayo = "abc";
+					  while (true)
+						{
+                int a = SYSgetSensorValue();
+                if (a > 200)
+                {
+                    SYSprint(1);
+                }
+                else if (a < 100)
+                {
+                    SYSprint(-1);
+                }
+                else
+                {
+                    SYSprint(0);
+                }
+						}
+        }
+
+    }
+}
