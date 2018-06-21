26491ed45e8e2568658bfb21fdc3e1e169eb7471
Jan Möhlmann <janmoehl@uni-bremen.de>
2018-06-21 14:46:52 +0200

    Add blink example code

---
+++ blink/Program.cs
@@ -0,0 +1,15 @@
+﻿class MainClass {
+	// hard compiled functions in flashed main.c
+	static public void SYSsetLED(bool i) {}
+	static public void SYSwait(int i) {}
+
+	// executed bytecode:
+	static public void Main() {
+		bool ledOn = true;
+		while (true) {
+			SYSsetLED(ledOn);
+			SYSwait(400000);
+			ledOn = !ledOn;
+		}
+	}
+}
